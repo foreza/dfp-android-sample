60449f3975b3e1049618b15cc55f72b5b8b1c027
Jason Chiu <jason.chiu@inmobi.com>
2019-08-02 16:43:14 -0700

    Amend main activity to support KW banner implementation

--- app/src/main/java/com/vartyr/dfpimabandroid/MainActivity.java
+++ app/src/main/java/com/vartyr/dfpimabandroid/MainActivity.java
@@ -13,6 +13,7 @@ import com.google.android.gms.ads.doubleclick.PublisherAdRequest;
 import com.google.android.gms.ads.doubleclick.PublisherAdView;
 
 import com.inmobi.ads.InMobiAudienceBidder;
+import com.inmobi.plugin.dfp.IMABCustomEventBanner;
 import com.inmobi.plugin.dfp.IMAudienceBidder;
 
 public class MainActivity extends Activity {
@@ -22,6 +23,9 @@ public class MainActivity extends Activity {
 
 
     private IMAudienceBidder.BidToken bannerBidToken;
+
+    private PublisherAdRequest.Builder IMAKWBadRequest;               // Used primarily by KW route
+
 
 
 
@@ -39,38 +43,26 @@ public class MainActivity extends Activity {
     }
 
 
+
+
+
+
     public void initSDK() {
-
-        // Init DFP SDK
-        MobileAds.initialize(this);
-
-
-
-
-        // Init IMAB SDK
-        InMobiAudienceBidder.initialize(this, "1017084");
-
+        MobileAds.initialize(this);                             // Init DFP SDK
+        InMobiAudienceBidder.initialize(this, "1017084");   // Init IMAB SDK
     }
-
 
 
 
     public void imabBannerRequest() {
 
-
         mPublisherAdView = findViewById(R.id.publisherAdView);
-
         PublisherAdRequest.Builder IMABadRequest = new PublisherAdRequest.Builder();
+        IMABadRequest.addTestDevice("DBA220EA435766BFB9DFB4CC7B7673A8");                    // Test Device ID
 
-        // Test Device ID
-        IMABadRequest.addTestDevice("DBA220EA435766BFB9DFB4CC7B7673A8");
+        IMAudienceBidder inMobiAudienceBidder = IMAudienceBidder.getInstance();             // Get singleton instance
 
-
-        // Get singleton instance
-        IMAudienceBidder inMobiAudienceBidder = IMAudienceBidder.getInstance();
-
-
-            bannerBidToken = inMobiAudienceBidder.createBidToken(this, "1055520", IMABadRequest, 320, 50, new IMAudienceBidder.IMAudienceBidderBannerListener() {
+        bannerBidToken = inMobiAudienceBidder.createBidToken(this, "1055520", IMABadRequest, 320, 50, new IMAudienceBidder.IMAudienceBidderBannerListener() {
             @Override
             public void onBidReceived(@NonNull  PublisherAdRequest.Builder builder) {
 
@@ -101,10 +93,7 @@ public class MainActivity extends Activity {
                         }
 
                         @Override
-                        public void onAdOpened() {
-                            // Code to be executed when an ad opens an overlay that
-                            // covers the screen.
-                        }
+                        public void onAdOpened() { }
 
                         @Override
                         public void onAdClicked() {
@@ -116,15 +105,10 @@ public class MainActivity extends Activity {
                         }
 
                         @Override
-                        public void onAdLeftApplication() {
-                            // Code to be executed when the user has left the app.
-                        }
+                        public void onAdLeftApplication() { }
 
                         @Override
-                        public void onAdClosed() {
-                            // Code to be executed when the user is about to return
-                            // to the app after tapping on an ad.
-                        }
+                        public void onAdClosed() { }
                     });
 
                     mPublisherAdView.loadAd(publisherAdRequest);
@@ -150,13 +134,95 @@ public class MainActivity extends Activity {
     }
 
 
+    public void imabBannerKWRequest() {
+
+        mPublisherAdView = findViewById(R.id.publisherAdView);
+        IMAKWBadRequest = new PublisherAdRequest.Builder();
+
+        IMAudienceBidder inMobiAudienceBidder = IMAudienceBidder.getInstance();             // Get singleton instance
+
+
+        bannerBidToken = inMobiAudienceBidder.createBidToken(this, "1055520", 320, 50, new IMAudienceBidder.IMAudienceBidderBannerKeywordListener() {
+            @Override
+            public void onBidReceived(IMAudienceBidder.IMABBidResponse imabBidResponse) {
+
+                // Get other bid information here
+                String bidKeyword = imabBidResponse.getBidKeyword();
+                String bidPrice = String.valueOf(imabBidResponse.getPrice());
+
+                Toast.makeText(MainActivity.this, "IMAb Banner KW onBidReceived with kw: " + bidKeyword + " and: " + bidPrice, Toast.LENGTH_LONG).show();
+
+                Bundle metadata = new Bundle();
+                metadata.putString(IMAudienceBidder.IMAB_PLACEMENT, "1055520");
+                IMAKWBadRequest.addCustomEventExtrasBundle(IMABCustomEventBanner.class, metadata);
+
+                IMAKWBadRequest.addTestDevice("DBA220EA435766BFB9DFB4CC7B7673A8");                    // Test Device ID
+                IMAKWBadRequest.addCustomTargeting(IMAudienceBidder.IMAB_KEY, imabBidResponse.getBidKeyword());
+
+
+                if (!bannerLoaded) {
+                    Toast.makeText(MainActivity.this, "IMAb KW Banner not yet loaded, loading into view", Toast.LENGTH_LONG).show();
+
+                    mPublisherAdView.setAdListener(new AdListener() {
+                        @Override
+                        public void onAdLoaded() {
+                            // Code to be executed when an ad finishes loading.
+
+                            Toast.makeText(MainActivity.this,"IMAb KW Banner onAdLoaded", Toast.LENGTH_LONG).show();
+
+                        }
+
+                        @Override
+                        public void onAdFailedToLoad(int errorCode) {
+                            // Code to be executed when an ad request fails.
+
+                            Toast.makeText(MainActivity.this,"IMAb KW Banner onAdFailedToLoad with error: " + errorCode, Toast.LENGTH_LONG).show();
+                        }
+
+                        @Override
+                        public void onAdOpened() { }
+
+                        @Override
+                        public void onAdClicked() {
+                            // Code to be executed when the user clicks on an ad.
+
+                            Toast.makeText(MainActivity.this,"IMAb KW Banner onAdClicked", Toast.LENGTH_LONG).show();
+
+
+                        }
+
+                        @Override
+                        public void onAdLeftApplication() { }
+
+                        @Override
+                        public void onAdClosed() { }
+                    });
+
+                    mPublisherAdView.loadAd(IMAKWBadRequest.build());
+                }
+
+            }
+
+            @Override
+            public void onBidFailed(Error error) {
+                IMAKWBadRequest.addTestDevice("DBA220EA435766BFB9DFB4CC7B7673A8");                    // Test Device ID
+                mPublisherAdView.loadAd(IMAKWBadRequest.build());
+            }
+        });
+
+        bannerBidToken.updateBid();
+
+    }
+
 
 
 
     public void configureAndLoadAdView(){
 
         mPublisherAdView = findViewById(R.id.publisherAdView);
-        PublisherAdRequest adRequest = new PublisherAdRequest.Builder().build();
+        PublisherAdRequest adRequest = new PublisherAdRequest.Builder()
+                .addTestDevice("DBA220EA435766BFB9DFB4CC7B7673A8")
+                .build();
         mPublisherAdView.loadAd(adRequest);
 
         // Attach Listeners
@@ -197,6 +263,34 @@ public class MainActivity extends Activity {
     }
 
 
+
+
+    // Example on how to load the DFP banner right into the view (uses XML)
+    public void loadDFPBanner(View view){
+
+//        mPublisherAdView.setAdUnitId("/6499/example/banner");
+
+        if (mPublisherAdView != null) {
+            mPublisherAdView.destroy();
+        }
+        configureAndLoadAdView();
+    }
+
+    public void loadDFPIMABBanner(View view){
+        if (mPublisherAdView != null) {
+            mPublisherAdView.destroy();
+        }
+
+        imabBannerRequest();
+    }
+
+
+    public void loadDFPIMABKWBanner(View view){
+        if (mPublisherAdView != null) {
+            mPublisherAdView.destroy();
+        }
+        imabBannerKWRequest();
+    }
 
 
 
